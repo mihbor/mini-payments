ff087bffe78b55facceb78ce0099055628bf7299
Michał Borowiecki <mihbor@users.noreply.github.com>
2022-12-10 18:37:05 +0000

    further refactor JoinChannel

--- src/jsMain/kotlin/logic/JoinChannel.kt
+++ src/jsMain/kotlin/logic/JoinChannel.kt
@@ -2,15 +2,45 @@ package logic
 
 import Channel
 import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
 import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonArray
 import logic.JoinChannelEvent.*
 import ltd.mbor.minimak.*
+import scope
 import kotlin.js.Date
 
 
 enum class JoinChannelEvent{
-  SCRIPTS_DEPLOYED, TRIGGER_TX_SIGNED, SETTLEMENT_TX_SIGNED, CHANNEL_PERSISTED, CHANNEL_PUBLISHED
+  SCRIPTS_DEPLOYED, SIGS_RECEIVED, TRIGGER_TX_SIGNED, SETTLEMENT_TX_SIGNED, CHANNEL_PERSISTED, CHANNEL_PUBLISHED, CHANNEL_UPDATED, CHANNEL_UPDATED_ACKED
+}
+
+fun joinChannel(
+  myKeys: Channel.Keys,
+  event: (JoinChannelEvent, Channel?) -> Unit = { _, _ -> }
+) {
+  var channel: Channel? = null
+  subscribe(myKeys).onEach { msg ->
+    console.log("tx msg", msg)
+    
+    val splits = msg.split(";")
+    if (splits[0].startsWith("TXN_UPDATE")) {
+      val isAck = splits[0].endsWith("_ACK")
+      channel = channel!!.update(isAck, updateTx = splits[1], settleTx = splits[2])
+      event(if (isAck) CHANNEL_UPDATED_ACKED else CHANNEL_UPDATED, channel)
+    } else {
+      val timeLock = splits[0].toInt()
+      val theirKeys = Channel.Keys(splits[1], splits[2], splits[3])
+      val triggerTx = splits[4]
+      val settlementTx = splits[5]
+      event(SIGS_RECEIVED, null)
+      joinChannel(myKeys, theirKeys, triggerTx, settlementTx, timeLock, event)
+    }
+  }.onCompletion {
+    console.log("completed")
+  }.launchIn(scope)
 }
 
 suspend fun joinChannel(
@@ -19,36 +49,28 @@ suspend fun joinChannel(
   triggerTx: String,
   settlementTx: String,
   timeLock: Int,
-  event: (JoinChannelEvent) -> Unit = {}
+  event: (JoinChannelEvent, Channel?) -> Unit = { _, _ -> }
 ): Channel {
   val myAddress = MDS.getAddress()
   multisigScriptAddress = MDS.deployScript(triggerScript(theirKeys.trigger, myKeys.trigger))
   eltooScriptAddress = MDS.deployScript(eltooScript(timeLock, theirKeys.update, myKeys.update, theirKeys.settle, myKeys.settle))
-  event(SCRIPTS_DEPLOYED)
+  event(SCRIPTS_DEPLOYED, null)
   
   val triggerTxId = newTxId()
   val outputs = MDS.importTx(triggerTxId, triggerTx)["outputs"]!!.jsonArray.map { json.decodeFromJsonElement<Coin>(it) }
   val (amount, tokenId) = outputs.find { it.address == eltooScriptAddress }!!.let { it.tokenAmount to it.tokenId }
   val signedTriggerTx = signAndExportTx(triggerTxId, myKeys.trigger)
-  event(TRIGGER_TX_SIGNED)
+  event(TRIGGER_TX_SIGNED, null)
   
   val settlementTxId = newTxId()
   val importedSettlementTx = MDS.importTx(settlementTxId, settlementTx)
   val output = json.decodeFromJsonElement<Coin>(importedSettlementTx["outputs"]!!.jsonArray.first())
   val theirAddress = output.miniAddress
   val signedSettlementTx = signAndExportTx(settlementTxId, myKeys.settle)
-  event(SETTLEMENT_TX_SIGNED)
+  event(SETTLEMENT_TX_SIGNED, null)
 
   val channelId = insertChannel(tokenId, amount, myKeys, theirKeys, signedTriggerTx, signedSettlementTx, timeLock, multisigScriptAddress, eltooScriptAddress, myAddress, theirAddress)
-  event(CHANNEL_PERSISTED)
-
-  publish(
-    theirKeys,
-    listOf(myAddress, signedTriggerTx, signedSettlementTx).joinToString(";")
-  )
-  event(CHANNEL_PUBLISHED)
-
-  return Channel(
+  val channel = Channel(
     id = channelId,
     sequenceNumber = 0,
     status = "OFFERED",
@@ -69,4 +91,13 @@ suspend fun joinChannel(
     eltooAddress = eltooScriptAddress,
     updatedAt = Date.now().toLong()
   )
+  event(CHANNEL_PERSISTED, channel)
+
+  publish(
+    theirKeys,
+    listOf(myAddress, signedTriggerTx, signedSettlementTx).joinToString(";")
+  )
+  event(CHANNEL_PUBLISHED, channel)
+
+  return channel
 }
--- src/jsMain/kotlin/ui/RequestChannel.kt
+++ src/jsMain/kotlin/ui/RequestChannel.kt
@@ -3,9 +3,6 @@ package ui
 import Channel
 import androidx.compose.runtime.*
 import kotlinx.browser.document
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
 import logic.*
 import logic.JoinChannelEvent.*
 import org.jetbrains.compose.web.css.px
@@ -14,13 +11,10 @@ import org.jetbrains.compose.web.dom.Br
 import org.jetbrains.compose.web.dom.Progress
 import org.jetbrains.compose.web.dom.Text
 import org.w3c.dom.HTMLCanvasElement
-import scope
 
 @Composable
 fun RequestChannel() {
   var myKeys by remember { mutableStateOf(Channel.Keys("", "", "")) }
-  var theirKeys by remember { mutableStateOf(Channel.Keys("", "", "")) }
-  var timeLock by remember { mutableStateOf(10) }
   
   var triggerTxStatus by remember { mutableStateOf("") }
   var updateTxStatus by remember { mutableStateOf("") }
@@ -42,37 +36,31 @@ fun RequestChannel() {
     ) { error ->
       if (error != null) console.error(error)
       else {
-        subscribe(myKeys).onEach { msg ->
-          console.log("tx msg", msg)
-          val splits = msg.split(";")
-          if (splits[0].startsWith("TXN_UPDATE")) {
-            channel = channel!!.update(splits[0].endsWith("ACK"), updateTx = splits[1], settleTx = splits[2])
-            updateTxStatus += "Update transaction ${if (splits[0].endsWith("ACK")) "ack " else ""}received. "
-          } else {
-            timeLock = splits[0].toInt()
-            theirKeys = Channel.Keys(splits[1], splits[2], splits[3])
-            val triggerTx = splits[4]
-            val settlementTx = splits[5]
-            triggerTxStatus = "Trigger transaction received"
-            settlementTxStatus = "Settlement transaction received"
-            progressStep++
-            
-            channel = joinChannel(myKeys, theirKeys, triggerTx, settlementTx, timeLock) {
-              progressStep++
-              when (it) {
-                TRIGGER_TX_SIGNED -> triggerTxStatus += " and signed"
-                SETTLEMENT_TX_SIGNED -> settlementTxStatus += " and signed"
-                CHANNEL_PUBLISHED -> {
-                  triggerTxStatus += " and sent back."
-                  settlementTxStatus += " and sent back."
-                }
-                else -> {}
-              }
+        joinChannel(myKeys) { event, newChannel ->
+          progressStep++
+          when (event) {
+            SIGS_RECEIVED -> {
+              triggerTxStatus = "Trigger transaction received"
+              settlementTxStatus = "Settlement transaction received"
             }
+            TRIGGER_TX_SIGNED -> triggerTxStatus += " and signed"
+            SETTLEMENT_TX_SIGNED -> settlementTxStatus += " and signed"
+            CHANNEL_PUBLISHED -> {
+              channel = newChannel
+              triggerTxStatus += " and sent back."
+              settlementTxStatus += " and sent back."
+            }
+            CHANNEL_UPDATED -> {
+              channel = newChannel
+              updateTxStatus += "Update transaction received. "
+            }
+            CHANNEL_UPDATED_ACKED -> {
+              channel = newChannel
+              updateTxStatus += "Update transaction ack received. "
+            }
+            else -> {}
           }
-        }.onCompletion {
-          console.log("completed")
-        }.launchIn(scope)
+        }
       }
     };Unit
   }
